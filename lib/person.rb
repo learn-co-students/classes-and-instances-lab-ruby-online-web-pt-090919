e2ccef5e79e213d59c3b5c6d375422acbea437bb
Jalysa Reason <jalysareason@gmail.com>
2019-09-24 18:30:22 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+def class definition
+  class definition(person.new)
+    adele_goldberg = (person.new)
+  end
+  
+  def class definition
+    class definition(person.new)
+      alan_kay = (person.new)
+    end
