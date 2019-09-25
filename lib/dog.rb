16a5944c552d762c1175a767f459854fc52689c6
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-25 20:11:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog #add class definition for Dog class
+
+end
+
+#make 3 dogs - instanitiate 3 nstaces od god
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
+
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+# define a `Person` class 
+class Person
+end
+
+#Make 2 people
+adele_goldberg = Person.new
+alan_kay = Person.new
