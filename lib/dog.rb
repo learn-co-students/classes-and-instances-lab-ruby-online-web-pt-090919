c8bd7e4c090ca57dd30d3af506a2de3e27f36a8c
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-25 20:12:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog #add class definition for Dog class
 
 end
 
-#make 3 dogs - instanitiate 3 nstaces od god
+#make 3 dogs - instanitiate 3 intsances of dog
 fido = Dog.new
 
 snoopy = Dog.new
